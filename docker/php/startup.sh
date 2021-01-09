d141ca76178eaeb9064c126800a66069b8b810d0
fyui001 <yun.cod@gmail.com>
2021-01-10 01:53:52 +0900

    リファクタリング

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -2,20 +2,13 @@ cd /code
 # composer
 composer install
 
-php artisan key:generate
-
 # cache clear
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-# database mingrate
-php artisan migrate
-php artisan db:seed
-
 chmod -R 777 /code/storage bootstrap/cache
 
 echo "starting php-fpm"
-exec $@
-zZ
+php-fpm -F
